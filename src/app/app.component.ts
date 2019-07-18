835d9c1bcdbce13100dc761bd93c565f91e5914b
gjester18 <gjester18@gmail.com>
2019-07-18 03:06:45 -0700

    done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,6 +6,56 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  
-  pageTitle : String = "My App App";
+
+  pageTitle: String = "";
+  format: String = "MMM d, y, h:mm:ss a";
+
+  job = {
+    jobTitle: "Web Designer",
+    employer: "Yoda Inc.",
+    icon: "/assets/img/sample_employer_icon.png",
+    compensation: {
+      type: "Monthly",
+      amount: 1500
+    },
+    applicationDeadline: "2018-11-01T09:00:00.000Z",
+    location: "Mountain View",
+    description: `We are a new Fast Casual Restaurant here in the Dhaka on the corner 
+    of Gulshan ave! We have a few more positions left for energetic, enthusiastic web designer
+     to work full time for the restaurant. Please carefully review the following qualifications and
+     responsibilities before submitting an application.`,
+    qualifications: ["3 years relevant work experience required",
+      "High school or higher level of education",
+      "Speak English",
+      "Professional reference needed"],
+
+    responsibilities: ["A web designer/developer is responsible for the design, layout and coding of a website.",
+      "They are involved with the technical and graphical aspects of a website; how the site works and how it looks. ",
+      "They can also be involved with the maintenance and update of an existing site."]
+  };
+
+  @ViewChild('jobTitle') jobTitle : ElementRef;
+  interval : any;
+
+  ngOnInit(){
+    this.interval = setInterval(()=>{
+      let bounding = this.jobTitle.nativeElement.getBoundingClientRect();
+      if (
+        bounding.top >= 0 &&
+        bounding.left >= 0 &&
+        bounding.right <= (window.innerWidth || document.documentElement.clientWidth) &&
+        bounding.bottom <= (window.innerHeight || document.documentElement.clientHeight)
+      ) {
+        this.pageTitle = "";
+      } else {
+        this.pageTitle = this.job.jobTitle;
+      }
+    },100);
+  }
+
+  ngOnDestroy(){
+    clearInterval(this.interval);
+  }
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MatToolbarModule} from '@angular/material';
+import {MatToolbarModule, MatCardModule, MatButtonModule, MatDividerModule, MatListModule} from '@angular/material';
+import { LineClampComponent } from './line-clamp/line-clamp.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LineClampComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    MatToolbarModule
+    MatToolbarModule,
+    MatCardModule,
+    MatButtonModule,
+    MatDividerModule,
+    MatListModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
